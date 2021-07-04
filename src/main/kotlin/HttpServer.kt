b56b275bf54d91d8400dd3be4f9e60e39412eccd
Karol Nowak <mail@karolnowak.net>
2021-07-04 22:06:46 +0200

    Allow only GET method

--- src/main/kotlin/HttpServer.kt
+++ src/main/kotlin/HttpServer.kt
@@ -1,26 +1,61 @@
 import java.io.OutputStream
 import java.net.ServerSocket
+import java.net.Socket
 import java.nio.file.Files
 import java.nio.file.Paths
 
-class HttpServer(private val filePath: String) {
+private const val EOL = "\r\n"
+
+class HttpServer(filePath: String) {
+    private var fileBytes = getFileBytes(filePath)
+
+    private fun getFileBytes(filePath: String): ByteArray {
+        val fileUrl = HttpServer::class.java.getResource(filePath)
+        return Files.readAllBytes(Paths.get(fileUrl.toURI()))
+    }
 
     fun start() {
-        val fileUrl = HttpServer::class.java.getResource(filePath)
-        val fileBytes: ByteArray = Files.readAllBytes(Paths.get(fileUrl.toURI()))
-        Thread { serveFileOnce(fileBytes) }.start()
+        Thread { serveFile() }.start()
     }
 
-    private fun statusLine() = "HTTP/1.1 200 spoko\r\n"
-    private fun headers(fileContent: ByteArray) = "Content-Length: ${fileContent.size}\r\n\r\n"
-
-
-    private fun serveFileOnce(fileBytes: ByteArray) {
-        ServerSocket(80).accept().use { socket ->
-            val outputStream: OutputStream = socket.getOutputStream()
-            val response = statusLine() + headers(fileBytes)
-            outputStream.write(response.toByteArray() + fileBytes)
-            println(response)
+    private fun serveFile() {
+        val socket = ServerSocket(80, 0) // backlog 0 seems to be ignored
+        while (true) {
+            runCatching {
+                socket.accept().use { socket ->
+                    val request = socket.readRequest()
+                    val outputStream: OutputStream = socket.getOutputStream()
+                    val response: Response = respondTo(request)
+                    outputStream.write(response.asBytes())
+                    println(response)
+                }
+            }
         }
     }
+
+    private fun respondTo(request: Request) =
+        when (request.method) {
+            "GET" -> Response(200, "Spoko", fileBytes.decodeToString())
+            else -> Response(405, "Nope")
+        }
+}
+
+private fun Socket.readRequest(): Request {
+    val requestLine = getInputStream().bufferedReader().readLine() // liberal in EOL recognition
+    val tokens = requestLine.split(" ")
+    require(tokens.size == 3)
+    return Request(tokens[0].uppercase(), tokens[1].uppercase(), tokens[2].uppercase())
+}
+
+internal data class Request(val method: String, val requestTarget: String, val httpVersion: String)
+internal data class Response(val statusCode: Int, val reasonPhrase: String, val responseBody: String = "") {
+    private fun isSuccessful() = statusCode in (100..399)
+
+    override fun toString(): String {
+        val statusLine = "HTTP/1.1 $statusCode $reasonPhrase"
+        val headers = if (isSuccessful()) "Content-Length: ${responseBody.toByteArray().size}" else "Accept: GET"
+        return statusLine + EOL + headers + EOL + EOL + if (isSuccessful()) responseBody + EOL else ""
+    }
+
+    fun asBytes() = toString().toByteArray()
 }
--- src/test/groovy/HttpServerTest.groovy
+++ src/test/groovy/HttpServerTest.groovy
@@ -1,14 +1,27 @@
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
+import spock.lang.Shared
 import spock.lang.Specification
+import spock.lang.Subject
+
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS
 
 class HttpServerTest extends Specification {
-    OkHttpClient client = new OkHttpClient()
+    @Subject
+    @Shared
+    HttpServer server = new HttpServer("file")
+
+    OkHttpClient client = new OkHttpClient.Builder().callTimeout(100, MILLISECONDS)
+            .readTimeout(100, MILLISECONDS)
+            .build()
+
+    def "setupSpec"() {
+        server.start()
+    }
 
     def "should serve file"() {
-        given:
-            new HttpServer("file").start()
         when:
             Response result = client.newCall(new Request.Builder()
                     .get().url("http://localhost").build()).execute()
@@ -16,5 +29,25 @@ class HttpServerTest extends Specification {
             result.body().string() == """this is file content
                                         |second line""".stripMargin('|')
             // server doesn't add CR
+    }
+
+    def "should serve file multiple times"() {
+        when:
+            10.times {
+                client.newCall(new Request.Builder()
+                        .get().url("http://localhost").build()).execute()
+            }
+        then:
+            noExceptionThrown()
+    }
+
+    def "should return 405 and Allow header when request uses method other than GET"() {
+        when:
+            Response result = client.newCall(new Request.Builder()
+                    .delete().url("http://localhost").build()).execute()
+        then:
+            result.code() == 405
+            result.header("Accept") == "GET"
+            result.body().string().isEmpty()
     }
 }
